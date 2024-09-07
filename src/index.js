a72c4cd28f9522a213e49befb01ed155a3b3459a
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2024-09-07 23:56:11 +0530

    Added to routers to complete api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { City } = require('./models/index');
 const { PORT } = require('./config/serverConfig');
+const ApiRoutes = require('./routes/index');
+
 const CityRepository = require('./repository/city-repository');
 const setupAndStartServer = async () => {
 
@@ -11,10 +13,12 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.use('/api', ApiRoutes);
+
     app.listen(PORT, () => {
         console.log(`server started at ${PORT}`);
-        const repo = new CityRepository();
-        repo.createCity({name: "New Delhi"});// 3rd lecture ends.
+        // const repo = new CityRepository();
+        // repo.createCity({name: "New Delhi"});// 3rd lecture ends.
     });
 }
 
